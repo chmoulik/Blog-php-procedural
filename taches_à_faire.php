c2a1c2219ea61bb35748eddf79239643efaa9167
Simon <105653650+chmoulik@users.noreply.github.com>
2024-03-26 16:10:23 +0100

    Update taches_à_faire.php

--- taches_à_faire.php
+++ taches_à_faire.php
@@ -1,20 +1,19 @@
 <?php
 
-//arranger , car il y a itération du dernier commentaire a chaque refreh de la page des commentaires.
+//arranger , car il y a itération du dernier commentaire a chaque refresh de la page des commentaires.
 
 //faire qu'on peut supprimer un user sans supprimer ses articles si c'est un Amin et commentaire si c'est un user normal.
 
 
-// CRD  des utilisateurs (uniqement le responsable du blog)
-// CRUD pour les articles (uniqement pour l'auteur ses propre article et le responsable du blog)
-// D pour les commentaires (uniqement le responsable du blog)
+// D  des utilisateurs (uniquement le responsable du blog)
+// UD pour les articles (uniquement pour l'auteur ses propres articles et le responsable du blog)
+// D pour les commentaires (uniquement le responsable du blog)
 
 
-//voir si tout est affiché lors de l'ajout d'article/commentaire/insciption...
+// voir si tout est affiché lors de l'ajout d'article/commentaire/insciption...
 
-//vérifier si le mail existe déjà avant de pouvoir s'inscrire.
+//vérifier avec du code php aussi que le mail soit unique avant de pouvoir s'inscrire .
 
 //vérifier que le commentaire n'existe pas déjà.
-
 
 // DEBUGER : le htmlspecialchars affiche les balise dans titre/l'article
